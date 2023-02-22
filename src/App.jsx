68a407c4d764d9be45136b3f894a50a9ff4b6557
Lee <s110919016@stu.ntue.edu.tw>
2023-02-23 00:22:44 +0800

    利用yarn安裝bootstrap套件

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 
-import React from 'react';
-import { ReactDOM } from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './bootstrap.min.css'
-import './index.css'
-// import './App.css';
 
 function App() {
   return (
